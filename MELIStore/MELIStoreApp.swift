5b5a35a34ac80501552642653fc55241b1ed9905
Andres Duque <andresduke024@gmail.com>
2025-04-05 08:28:16 -0500

    Initial Commit

---
+++ MELIStore/MELIStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  MELIStoreApp.swift
+//  MELIStore
+//
+//  Created by Andres Duque on 5/04/25.
+//
+
+import SwiftUI
+
+@main
+struct MELIStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
